a0f173dc64e12f4c4dc6f19fc9ef093302acabae
numb86 <86numb@gmail.com>
2018-10-01 18:46:36 +0900

    ESLint でエラーが出ない形に書き換えた。

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import Vue from 'vue';
 import App from './components/App.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('app'));
-  /* eslint-disable no-new */
   new Vue({
-    el,
     render: h => h(App),
-  });
+  }).$mount('#app');
 });
