147cd8b24a614654c2707b843ae332d9a54747d7
numb86 <86numb@gmail.com>
2018-12-04 23:37:40 +0900

    ルーティングを実装した。

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,23 +1,3 @@
 <template>
-  <div>
-    <p>{{ message }}</p>
-  </div>
+  <router-view />
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      message: 'Hello Vue!',
-    };
-  },
-};
-</script>
-
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-  color: red;
-}
-</style>
