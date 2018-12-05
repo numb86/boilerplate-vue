8b8e60aedb418d6ff67b01c9818d75f0350aea5c
numb86 <86numb@gmail.com>
2018-12-05 19:53:11 +0900

    コンポーネント側で Vuex を利用するようにした。

--- src/components/PageFoo.vue
+++ src/components/PageFoo.vue
@@ -1,3 +1,27 @@
 <template>
-  <div><p>This is foo page.</p></div>
+  <div>
+    <p>This is foo page.</p>
+    <p>Count: {{ count }}</p>
+    <p><button @click.prevent="increment({value: 1})">increment</button></p>
+    <p>
+      <button @click.prevent="asyncIncrement({value: 2})">
+        async increment
+      </button>
+    </p>
+  </div>
 </template>
+
+<script>
+import {mapState, mapGetters, mapMutations, mapActions} from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['count']),
+    ...mapGetters(['message']),
+  },
+  methods: {
+    ...mapMutations(['increment']),
+    ...mapActions(['asyncIncrement']),
+  },
+};
+</script>
